e8b198caed79ff1d19863c98d69dadd1172d9529
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-09-25 23:13:15 +0300

    tein staticseista oman komponentin

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,55 +1,39 @@
 import { useState } from "react";
+import Header from "../src/components/Header";
+import Statistic from "../src/components/Statistic";
 
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const setGoodVote = (goodVote) => {
-    console.log("good value is now", goodVote);
-    setGood(goodVote);
+  const setGoodVote = () => {
+    const setGoodVote = good + 1;
+    setGood(setGoodVote);
   };
 
-  const setNeutralVote = (neutralVote) => {
-    console.log("neutral value is now", neutralVote);
-    setNeutral(neutralVote);
+  const setNeutralVote = () => {
+    const setNeutralVote = neutral + 1;
+    setNeutral(setNeutralVote);
   };
 
-  const setBadVote = (badVote) => {
-    console.log("bad vote value is now", badVote);
-    setBad(badVote);
+  const setBadVote = () => {
+    const setBadVote = bad + 1;
+    setBad(setBadVote);
   };
-
-  const total = good + neutral + bad;
-  const average = total > 0 ? (good - bad) / total : 0;
-  const positive = total > 0 ? (good / total) * 100 : 0;
 
   return (
-    <div>
-      <h1>Give Feedback!</h1>
-      <Button handleClick={() => setGoodVote(good + 1)} text="Good" />
-      <Button handleClick={() => setNeutralVote(neutral + 1)} text="Neutral" />
-      <Button handleClick={() => setBadVote(bad + 1)} text="Bad" />
-
-      <h1>Statistics!</h1>
-      {total === 0 ? (
-        <div>no feedback given yet</div>
-      ) : (
-        <div>
-          <p>Good: {good}</p>
-          <p>Neutral: {neutral}</p>
-          <p>Bad: {bad}</p>
-          <p>All votes: {total}</p>
-          <p>Average score is: {average}</p>
-          <p>Positive score is: {positive} %</p>
-        </div>
-      )}
-    </div>
+    <>
+      <Header />
+      <Button handleClick={setGoodVote} text="Good" />
+      <Button handleClick={setNeutralVote} text="Neutral" />
+      <Button handleClick={setBadVote} text="Bad" />
+      <Statistic good={good} neutral={neutral} bad={bad} />
+    </>
   );
 };
 
---
+++ unicafe/src/components/Statistic.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Statistic({ good, neutral, bad }) {
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positivePrecentage = total === 0 ? 0 : (good / total) * 100;
+
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positivePrecentage}</p>
+    </div>
+  );
+}
+
+export default Statistic;
