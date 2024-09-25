7ce1698da46bbf7801ef6f9dbb0ebaa9a1ea0a2c
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-09-25 14:02:13 +0300

    unicafe step1 done

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,27 +1,40 @@
-import React, { useState } from "react";
-
-const Display = (props) => <div>{props.value}</div>;
+import { useState } from "react";
 
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
 
 const App = () => {
-  const [value, setValue] = useState(10);
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const setToValue = (newValue) => {
-    console.log("value now", newValue);
-    setValue(newValue);
+  const setGoodVote = (goodVote) => {
+    console.log("good value is now", goodVote);
+    setGood(goodVote);
+  };
+
+  const setNeutralVote = (neutralVote) => {
+    console.log("neutral value is now", neutralVote);
+    setNeutral(neutralVote);
+  };
+
+  const setBadVote = (badVote) => {
+    console.log("bad vote value is now", badVote);
+    setBad(badVote);
   };
 
   return (
     <div>
-      <div>
-        <Display value={value} />
-        <Button handleClick={() => setToValue(1000)} text="thousand" />
-        <Button handleClick={() => setToValue(0)} text="reset" />
-        <Button handleClick={() => setToValue(value + 1)} text="increment" />
-      </div>
+      <h1>Give Feedback!</h1>
+      <Button handleClick={() => setGoodVote(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutralVote(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBadVote(bad + 1)} text="Bad" />
+      <h1>Statistics!</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   );
 };
